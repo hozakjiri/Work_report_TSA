aaed96acc8e871283493c7a280f5122a7055f63e
Vanderhell <Vanderhell@gmail.com>
2021-10-26 19:25:36 +0200

    pridanie enums do projektu

---
+++ WorkReportWPF/Enums/LevelEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkReportWPF.Enums
+{
+
+    public enum LevelEnum
+    {
+        Level1 = 0,
+        Level2 = 1,
+        Level3 = 2,
+        Level4 = 3,
+        Level5 = 4,
+        Admin = 5
+    }
+}
