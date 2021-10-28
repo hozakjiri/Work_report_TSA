75d50574acdd55e96d8492cd0f9359439a2870ef
Vanderhell <Vanderhell@gmail.com>
2021-10-28 20:22:32 +0200

    pridane formy pre výkazy

--- WorkReportWPF/Forms/LIstOfModifications.xaml.cs
+++ WorkReportWPF/Forms/LIstOfModifications.xaml.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using WorkReportWPF.Forms.ListOfModification;
 
 namespace WorkReportWPF
 {
@@ -23,6 +12,21 @@ namespace WorkReportWPF
         public LIstOfModifications()
         {
             InitializeComponent();
+        }
+
+        private void Click_Overview(object sender, RoutedEventArgs e)
+        {
+            frameModification.Content = new OverviewModification();
+        }
+
+        private void Click_Add(object sender, RoutedEventArgs e)
+        {
+            frameModification.Content = new AddModification();
+        }
+
+        private void Click_Send(object sender, RoutedEventArgs e)
+        {
+            frameModification.Content = new SendModification();
         }
     }
 }
