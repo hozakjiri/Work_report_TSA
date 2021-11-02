999609ad2e42a21ab30a70bccc09aff742ea6311
Vanderhell <Vanderhell@gmail.com>
2021-11-02 20:21:03 +0100

    pridanie kalendáru na MainWindow

--- WorkReportWPF/Forms/MainScreen.xaml.cs
+++ WorkReportWPF/Forms/MainScreen.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Threading;
 
 namespace WorkReportWPF
 {
@@ -10,6 +13,40 @@ namespace WorkReportWPF
         public MainScreen()
         {
             InitializeComponent();
+
+            DispatcherTimer timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(1);
+            timer.Tick += timer_Tick;
+            timer.Start();
         }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Date_Text.Text = DateTime.Now.ToShortDateString();
+            Day_Text.Text = DateTime.Now.DayOfWeek.ToString();
+            Time_Text.Text = DateTime.Now.ToShortTimeString();
+            Week_Text.Text = GetWeekNumber(DateTime.Now).ToString();
+        }
+
+        static int GetWeekNumberOfMonth(DateTime date)
+        {
+            date = date.Date;
+            DateTime firstMonthDay = new DateTime(date.Year, date.Month, 1);
+            DateTime firstMonthMonday = firstMonthDay.AddDays((DayOfWeek.Monday + 7 - firstMonthDay.DayOfWeek) % 7);
+            if (firstMonthMonday > date)
+            {
+                firstMonthDay = firstMonthDay.AddMonths(-1);
+                firstMonthMonday = firstMonthDay.AddDays((DayOfWeek.Monday + 7 - firstMonthDay.DayOfWeek) % 7);
+            }
+            return (date - firstMonthMonday).Days / 7 + 1;
+        }
+
+        static int GetWeekNumber(DateTime date)
+        {
+            CultureInfo ciCurr = CultureInfo.CurrentCulture;
+            int weeknum = ciCurr.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
+            return weeknum;
+        }
+
     }
 }
