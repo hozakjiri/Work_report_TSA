7199cfa3d01860c6d8d8d41e3f7a1cba867e76fe
hozakjiri <45700560+hozakjiri@users.noreply.github.com>
2021-11-03 19:56:45 +0100

    upravy

---
+++ WorkReportWPF/Forms/ListOfModification/AddRemoteControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WorkReportWPF.Forms.ListOfModification
+{
+    /// <summary>
+    /// Interakční logika pro AddRemoteControl.xaml
+    /// </summary>
+    public partial class AddRemoteControl : Window
+    {
+        public AddRemoteControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
