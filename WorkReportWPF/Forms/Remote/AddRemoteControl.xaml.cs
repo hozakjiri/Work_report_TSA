e465d828f653b2892e4fcbdde3ad8acb71f1d5b4
Vanderhell <Vanderhell@gmail.com>
2021-11-04 19:18:40 +0100

    prerobeny remote

---
+++ WorkReportWPF/Forms/Remote/AddRemoteControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace WorkReportWPF.Forms.Remote
+{
+    /// <summary>
+    /// Interaction logic for AddRemoteControl.xaml
+    /// </summary>
+    public partial class AddRemoteControl : Page
+    {
+        public AddRemoteControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
